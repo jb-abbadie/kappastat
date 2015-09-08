1a87fb7f080168ab40f8c8c5366d8ebcd2dab332
grsakea <grsakea@users.noreply.github.com>
2015-09-08 16:50:27 +0200

    Add test to storage backend

---
+++ main/storage_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStorage(t *testing.T) {
+	db := setupStorage()
+
+	ce := db.C("chat_entries")
+	vc := db.C("viewer_count")
+
+	storeChatEntry(ce, ChatEntry{"testChan", "testSender", time.Now(), "testText"})
+	storeViewerCount(vc, ViewerCount{"testChan", time.Now(), 42})
+
+	count, err := ce.Count()
+	if count != 1 {
+		t.Error("Chat Entry storage invalid")
+	}
+	if err != nil {
+		t.Error("Chat Entry fetching error")
+	}
+	count, err = vc.Count()
+	if count != 1 {
+		t.Error("ViewerCount storage invalid")
+	}
+	if err != nil {
+		t.Error("ViewerCount fetching error")
+	}
+}
