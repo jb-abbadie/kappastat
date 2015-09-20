6f8ce5961fae8ebdce72ab576a4cb8fdb6d4067d
grsakea <grsakea@users.noreply.github.com>
2015-09-21 00:11:58 +0200

    Add stat template

---
+++ static/viewer.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#curve_chart').height('80%');
+    $('#curve_chart').width('100%');
+    $('#list_streams').hide();
+    $.getJSON("/api/following", listFollowing);
+
+    $('#list_streams').change( function() {
+        $('#curve_chart').fadeOut(500);
+        $.getJSON("/api/viewer/" + $('#list_streams option:selected').text(), function(data) {
+
+            var result = [];
+            result.push(['Time', 'Viewer']);
+            for(var i in data) {
+                result.push([new Date(data[i]['Time']), data[i]['Viewer']]);
+            }
+            $('#curve_chart').fadeIn(1500);
+            drawChart(result);
+        });
+    });
+});
+
