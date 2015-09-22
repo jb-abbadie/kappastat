09fef8e649c709f14e0126bcc42c933290b1d217
grsakea <grsakea@users.noreply.github.com>
2015-09-22 19:59:15 +0200

    Remove traces of following

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/grsakea/kappastat/backend"
-	"github.com/mrshankly/go-twitch/twitch"
-	"io/ioutil"
-	"net/http"
 )
 
 func main() {
@@ -14,31 +10,6 @@ func main() {
 
 func launchBackend() *backend.Controller {
 	c := backend.SetupController("twitch")
-	client := twitch.NewClient(&http.Client{})
-
-	data, err := ioutil.ReadFile("following")
-	if err == nil {
-		loadFollowing(c, data)
-		print(data)
-	} else {
-		opt := &twitch.ListOptions{
-			Limit:  10,
-			Offset: 0,
-		}
-		l, _ := client.Streams.List(opt)
-		for _, item := range l.Streams {
-			go c.AddStream(item.Channel.Name)
-		}
-	}
 	go c.Loop()
 	return c
-}
-
-func loadFollowing(c *backend.Controller, data []byte) {
-	var f []string
-	json.Unmarshal(data, &f)
-
-	for i := range f {
-		go c.AddStream(f[i])
-	}
 }
