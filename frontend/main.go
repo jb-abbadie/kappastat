6938b44bb96b0f0c057aafb1cc81c159fc8def48
grsakea <grsakea@users.noreply.github.com>
2015-09-23 17:19:07 +0200

    Remove launch backend from frontend

--- frontend/main.go
+++ frontend/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"github.com/grsakea/kappastat/backend"
-)
-
 func main() {
-	launchBackend()
 	launchFrontend()
-}
-
-func launchBackend() *backend.Controller {
-	c := backend.SetupController("twitch")
-	go c.Loop()
-	return c
 }
